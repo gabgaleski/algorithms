430b823e87e5ca199e4433beae6e6fe86a450d06
Gabriel Galeski <galeski35@gmail.com>
2023-09-21 11:58:29 -0300

    feat: implementa algoritmo para resolução de anagrama

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+
+    for i in range(len(word) // 2):
+        last_index = word[-i - 1]
+        if word[i] != last_index:
+            return False
+
+    return True
